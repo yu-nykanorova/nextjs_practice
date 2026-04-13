b4e1588d505d370057b809dabe4a1a6a36c84602
yu-nykanorova <juliyasos@ukr.net>
2026-04-13 19:05:18 +0300

    added simple form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-indigo-50 dark:bg-black sm:items-start">
+          <form action="" className="flex justify-center">
+              <input type="text" name={"name"} className="bg-white mr-2 rounded-md"/>
+              <button className="p-1 text-white bg-indigo-500 rounded-md">Submit</button>
+          </form>
       </main>
     </div>
   );
