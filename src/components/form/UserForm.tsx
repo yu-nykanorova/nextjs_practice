74bf0b13e02df64db8c559ffdc1ea191a484a3ca
yu-nykanorova <juliyasos@ukr.net>
2026-04-14 14:54:27 +0300

    ашÑfixed UI mistakes in UserForm component

--- src/components/form/UserForm.tsx
+++ src/components/form/UserForm.tsx
@@ -31,11 +31,11 @@ export const UserForm = () => {
             </div>
             <div className="w-full">
                 <input {...register("email")} placeholder="email" className="w-full px-2 py-1 bg-white mr-2 rounded-sm"/>
-                {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+                {errors.email && <p className="text-red-500">{errors.email.message}</p>}
             </div>
             <div className="w-full">
                 <input {...register("password")} placeholder="password" className="w-full px-2 py-1 bg-white mr-2 rounded-sm"/>
-                {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+                {errors.password && <p className="text-red-500">{errors.password.message}</p>}
             </div>
             <button type="submit" className="w-1/2 px-4 py-1 text-white bg-indigo-500 rounded-sm">Submit</button>
         </form>
